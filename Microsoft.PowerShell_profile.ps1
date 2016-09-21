5b739b67fd130113e6971420b41da0348c88516a
Steve Lasker <StevenLasker@Hotmail.com>
2016-09-20 18:19:19 -0700

    Set the window title to the current directory

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 # Save to: %userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 function prompt
 {
+    $host.UI.RawUI.WindowTitle = Get-Location
     Write-Host ("PS>") -nonewline -foregroundcolor Green -backgroundcolor Black
     return " "
 }
