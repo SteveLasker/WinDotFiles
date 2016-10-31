f2bace96e80fa11033b22e810c8291550c6544ad
Steve Lasker <StevenLasker@Hotmail.com>
2016-10-31 09:45:36 -0700

    added MKDIRs

--- updateMySettings.cmd
+++ updateMySettings.cmd
@@ -1,2 +1,4 @@
+MKDIR %userprofile%\Documents\WindowsPowerShell
 copy Microsoft.PowerShell_profile.ps1 %userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 /Y
+MKDIR %userprofile%\.docker
 copy config.json %userprofile%\.docker\config.json /Y
