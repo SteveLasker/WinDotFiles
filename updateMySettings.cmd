97c3800220199e7c30c31ab5e5b1975ed983e748
Steve Lasker <StevenLasker@Hotmail.com>
2017-01-19 13:46:09 -0800

    Add execution policy

--- updateMySettings.cmd
+++ updateMySettings.cmd
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Unrestricted
 MKDIR %userprofile%\Documents\WindowsPowerShell
 copy Microsoft.PowerShell_profile.ps1 %userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 /Y
 MKDIR %userprofile%\.docker
